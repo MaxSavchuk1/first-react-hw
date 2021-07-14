90c8a539ae697ded33dd92dbf8771bc8b5e59f8b
MaxSavchuk <avatar2945@gmail.com>
2021-07-14 22:08:59 +0300

    third commit

--- src/components/UserListHW/index.jsx
+++ src/components/UserListHW/index.jsx
@@ -11,65 +11,73 @@ import UserListItem from "../UserListItemHW";
 //  (Взаимодействие child -> parent: callback).
 
 const usersDb = [
-  {
-    id: 1,
-    firstName: "Ivan",
-    lastName: "Ivanov",
-    age: 19,
-    imgSrc: "https://randomuser.me/api/portraits/men/83.jpg",
-  },
-  {
-    id: 2,
-    firstName: "Petr",
-    lastName: "Petrov",
-    age: 24,
-    imgSrc: "https://randomuser.me/api/portraits/men/68.jpg",
-  },
-  {
-    id: 3,
-    firstName: "Alexey",
-    lastName: "Alexeenko",
-    age: 36,
-    imgSrc: "https://randomuser.me/api/portraits/men/8.jpg",
-  },
-  {
-    id: 4,
-    firstName: "Vera",
-    lastName: "Ivanova",
-    age: 21,
-    imgSrc: "https://randomuser.me/api/portraits/women/9.jpg",
-  },
-  {
-    id: 5,
-    firstName: "Dasha",
-    lastName: "Petrova",
-    age: 25,
-    imgSrc: "https://randomuser.me/api/portraits/women/10.jpg",
-  },
+	{
+		id: 1,
+		firstName: "Ivan",
+		lastName: "Ivanov",
+		age: 19,
+		imgSrc: "https://randomuser.me/api/portraits/men/83.jpg",
+	},
+	{
+		id: 2,
+		firstName: "Petr",
+		lastName: "Petrov",
+		age: 24,
+		imgSrc: "https://randomuser.me/api/portraits/men/68.jpg",
+	},
+	{
+		id: 3,
+		firstName: "Alexey",
+		lastName: "Alexeenko",
+		age: 36,
+		imgSrc: "https://randomuser.me/api/portraits/men/8.jpg",
+	},
+	{
+		id: 4,
+		firstName: "Vera",
+		lastName: "Ivanova",
+		age: 21,
+		imgSrc: "https://randomuser.me/api/portraits/women/9.jpg",
+	},
+	{
+		id: 5,
+		firstName: "Dasha",
+		lastName: "Petrova",
+		age: 25,
+		imgSrc: "https://randomuser.me/api/portraits/women/10.jpg",
+	},
 ];
 
 class UserList extends Component {
-  constructor(props) {
-    super(props);
+	constructor(props) {
+		super(props);
 
-    this.state = {
-      users: usersDb,
-    };
-  }
+		this.state = {
+			users: usersDb.map((u) => ({ ...u, isSelected: false })),
+		};
+	}
 
-  usersMap = (u, i) => {
-    const { users } = this.state;
-    const deleteUser = () => {
-      const newUsers = [...users];
-      newUsers.splice(i, 1);
-      this.setState({ users: newUsers });
-    };
-    return <UserListItem key={u.id} user={u} del={deleteUser} />;
-  };
+	usersMap = (u, i) => {
+		const { users } = this.state;
+		const selectedUsers = [...users];
+		const selectUser = () => {
+			selectedUsers[i].isSelected = !selectedUsers[i].isSelected;
+			this.setState({ users: selectedUsers });
+		};
+		const deleteUser = () => {
+			const newUsers = [...selectedUsers];
+			selectedUsers.splice(i, 1);
+			this.setState({ selectedUsers: newUsers });
+		};
 
-  render() {
-    const { users } = this.state;
-    return <ul>{users.map(this.usersMap)}</ul>;
-  }
+		return (
+			<UserListItem key={u.id} user={u} del={deleteUser} select={selectUser} />
+		);
+	};
+
+	render() {
+		const { users } = this.state;
+		return <ul>{users.map(this.usersMap)}</ul>;
+	}
 }
 export default UserList;
--- src/components/UserListItemHW/index.jsx
+++ src/components/UserListItemHW/index.jsx
@@ -1,23 +1,26 @@
 import React from "react";
 
 function UserListItem(props) {
-  const {
-    user: { id, firstName, lastName, age, imgSrc },
-    del,
-  } = props;
+	const {
+		user: { id, firstName, lastName, age, imgSrc, isSelected },
+		del,
+		select,
+	} = props;
 
-  return (
-    <li>
-      <img src={imgSrc} alt={id} />
-      <div className="userInfo">
-        <span>
-          {firstName} {lastName}
-        </span>
-        <span>{age} years old</span>
-      </div>
-      <button onClick={del}>X</button>
-    </li>
-  );
+	const selectedStyle = { boxShadow: "0px 0px 10px red" };
+
+	return (
+		<li style={isSelected ? selectedStyle : null} onClick={select}>
+			<img src={imgSrc} alt={id} />
+			<div className="userInfo">
+				<span>
+					{firstName} {lastName}
+				</span>
+				<span>{age} years old</span>
+			</div>
+			<button onClick={del}>X</button>
+		</li>
+	);
 }
 
 export default UserListItem;

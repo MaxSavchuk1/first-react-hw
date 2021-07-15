e1f7c8f9a1dc166d912299984f83264610ea0883
MaxSavchuk <avatar2945@gmail.com>
2021-07-15 14:03:34 +0300

    beta version

--- src/components/UserListHW/index.jsx
+++ src/components/UserListHW/index.jsx
@@ -1,76 +1,74 @@
 import React, { Component } from "react";
 import UserListItem from "../UserListItemHW";
 
-// Создать список пользователей (компоненты UsersListHW, UserListItemHW). (Взаимодействие parent -> child: props).
-// У пользователей есть такая инфа: id, firstName, lastName, age, imgSrc.
-// Застилить карточки пользователей (пример в приложении или по-своему).
-
-// Реализовать (одно или оба):
-// - * выбор пользователя (по клику на него). Выбранных пользователей стилить, например, бордером и фоном;
-// - * удаление пользователя (по клику на кнопке возле пользователя).
-//  (Взаимодействие child -> parent: callback).
-
 const usersDb = [
-	{
-		id: 1,
-		firstName: "Ivan",
-		lastName: "Ivanov",
-		age: 19,
-		imgSrc: "https://randomuser.me/api/portraits/men/83.jpg",
-	},
-	{
-		id: 2,
-		firstName: "Petr",
-		lastName: "Petrov",
-		age: 24,
-		imgSrc: "https://randomuser.me/api/portraits/men/68.jpg",
-	},
-	{
-		id: 3,
-		firstName: "Alexey",
-		lastName: "Alexeenko",
-		age: 36,
-		imgSrc: "https://randomuser.me/api/portraits/men/8.jpg",
-	},
-	{
-		id: 4,
-		firstName: "Vera",
-		lastName: "Ivanova",
-		age: 21,
-		imgSrc: "https://randomuser.me/api/portraits/women/9.jpg",
-	},
-	{
-		id: 5,
-		firstName: "Dasha",
-		lastName: "Petrova",
-		age: 25,
-		imgSrc: "https://randomuser.me/api/portraits/women/10.jpg",
-	},
+  {
+    id: 1,
+    firstName: "Ivan",
+    lastName: "Ivanov",
+    age: 19,
+    imgSrc: "https://randomuser.me/api/portraits/men/83.jpg",
+  },
+  {
+    id: 2,
+    firstName: "Petr",
+    lastName: "Petrov",
+    age: 24,
+    imgSrc: "https://randomuser.me/api/portraits/men/68.jpg",
+  },
+  {
+    id: 3,
+    firstName: "Alexey",
+    lastName: "Alexeenko",
+    age: 36,
+    imgSrc: "https://randomuser.me/api/portraits/men/8.jpg",
+  },
+  {
+    id: 4,
+    firstName: "Vera",
+    lastName: "Ivanova",
+    age: 21,
+    imgSrc: "https://randomuser.me/api/portraits/women/9.jpg",
+  },
+  {
+    id: 5,
+    firstName: "Dasha",
+    lastName: "Petrova",
+    age: 25,
+    imgSrc: "https://randomuser.me/api/portraits/women/10.jpg",
+  },
 ];
 
 class UserList extends Component {
-	constructor(props) {
-		super(props);
+  constructor(props) {
+    super(props);
 
-		this.state = {
-			users: usersDb.map((u) => ({ ...u, isSelected: false })),
-		};
-	}
+    this.state = {
+      users: usersDb, //оно и так всё работает О_о
+      // users: usersDb.map((u) => ({ ...u, isSelected: false })),
+    };
+  }
 
-	usersMap = (u, i) => {
-		const { users } = this.state;
-		const deleteUser = () => {
-			const newUsers = [...users];
-			newUsers.splice(i, 1);
-			this.setState({ users: newUsers });
-		};
+  usersMap = (u, i) => {
+    const { users } = this.state;
+    const newUsers = [...users];
+    const selectUser = () => {
+      newUsers[i].isSelected = !newUsers[i].isSelected; //наверно потому, что isSelected нет в объекте и оно undefined = false...
+      this.setState({ users: newUsers });
+    };
+    const deleteUser = () => {
+      newUsers.splice(i, 1);
+      this.setState({ users: newUsers });
+    };
 
-		return <UserListItem key={u.id} user={u} del={deleteUser} />;
-	};
+    return (
+      <UserListItem key={u.id} user={u} del={deleteUser} select={selectUser} />
+    );
+  };
 
-	render() {
-		const { users } = this.state;
-		return <ul>{users.map(this.usersMap)}</ul>;
-	}
+  render() {
+    const { users } = this.state;
+    return <ul>{users.map(this.usersMap)}</ul>;
+  }
 }
 export default UserList;
--- src/components/UserListItemHW/index.jsx
+++ src/components/UserListItemHW/index.jsx
@@ -1,25 +1,26 @@
 import React from "react";
 
 function UserListItem(props) {
-	const {
-		user: { id, firstName, lastName, age, imgSrc },
-		del,
-	} = props;
+  const {
+    user: { id, firstName, lastName, age, imgSrc, isSelected },
+    del,
+    select,
+  } = props;
 
-	const selectedStyle = { boxShadow: "0px 0px 10px red" };
+  const selectedStyle = { boxShadow: "0px 0px 10px red" };
 
-	return (
-		<li>
-			<img src={imgSrc} alt={id} />
-			<div className="userInfo">
-				<span>
-					{firstName} {lastName}
-				</span>
-				<span>{age} years old</span>
-			</div>
-			<button onClick={del}>X</button>
-		</li>
-	);
+  return (
+    <li style={isSelected ? selectedStyle : null} onClick={select}>
+      <img src={imgSrc} alt={id} />
+      <div className="userInfo">
+        <span>
+          {firstName} {lastName}
+        </span>
+        <span>{age} years old</span>
+      </div>
+      <button onClickCapture={del}>X</button>
+    </li>
+  );
 }
 
 export default UserListItem;

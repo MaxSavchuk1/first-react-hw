0350296336a63a4505b16f564b30feab05184f19
MaxSavchuk <avatar2945@gmail.com>
2021-08-03 16:03:24 +0300

    add date selection

---
+++ src/components/Calendar/SelectForm/index.jsx
@@ -0,0 +1,70 @@
+import React, { Component } from "react";
+import { getYear, getMonth } from "date-fns";
+
+class SelectForm extends Component {
+	constructor(props) {
+		super(props);
+
+		const { date } = props;
+
+		this.state = {
+			monthValue: getMonth(date),
+			yearValue: getYear(date),
+		};
+	}
+
+	sumbitHndlr = (e) => {
+		e.preventDefault();
+	};
+
+	dateChangeHndlr = (e) => {
+		this.setState({ [e.target.name]: e.target.value });
+	};
+
+	resetHndlr = (e) => {
+		const currentMonth = getMonth(new Date());
+		const currentYear = getYear(new Date());
+		this.setState({ monthValue: currentMonth, yearValue: currentYear });
+		this.props.dateChange(currentYear, currentMonth);
+	};
+
+	render() {
+		const { monthValue, yearValue } = this.state;
+		const { dateChange } = this.props;
+		return (
+			<form onSubmit={this.sumbitHndlr} onChange={this.dateChangeHndlr}>
+				<select name="monthValue" value={monthValue}>
+					<option value="0">January</option>
+					<option value="1">February</option>
+					<option value="2">March</option>
+					<option value="3">April</option>
+					<option value="4">May</option>
+					<option value="5">June</option>
+					<option value="6">July</option>
+					<option value="7">August</option>
+					<option value="8">September</option>
+					<option value="9">October</option>
+					<option value="10">November</option>
+					<option value="11">December</option>
+				</select>
+				<input
+					name="yearValue"
+					type="number"
+					value={yearValue}
+					onChange={this.dateChangeHndlr}
+				/>
+				<button
+					type="submit"
+					onClick={() => {
+						dateChange(yearValue, monthValue);
+					}}
+				>
+					Select
+				</button>
+				<button onClick={this.resetHndlr}>Reset</button>
+			</form>
+		);
+	}
+}
+
+export default SelectForm;
--- src/components/Calendar/index.jsx
+++ src/components/Calendar/index.jsx
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import Day from "./Day";
 import Month from "./Month";
 import styles from "./Calendar.module.scss";
+import SelectForm from "./SelectForm";
 
 class Calendar extends Component {
 	constructor(props) {
@@ -12,13 +13,20 @@ class Calendar extends Component {
 		};
 	}
 
+	dateChange = (yearValue, monthValue) => {
+		this.setState({ date: new Date(yearValue, monthValue) });
+	};
+
 	render() {
 		const { date } = this.state;
 		return (
-			<article className={styles.calendarContainer}>
-				<Day />
-				<Month date={date} />
-			</article>
+			<>
+				<SelectForm date={date} dateChange={this.dateChange} />
+				<article className={styles.calendarContainer}>
+					<Day />
+					<Month date={date} />
+				</article>
+			</>
 		);
 	}
 }

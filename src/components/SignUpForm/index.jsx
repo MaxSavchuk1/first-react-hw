e942e77188bf06d47fd3c91e097256f2e677ed60
MaxSavchuk <avatar2945@gmail.com>
2021-08-07 10:20:01 +0300

    sign up in process...

--- src/components/SignUpForm/index.jsx
+++ src/components/SignUpForm/index.jsx
@@ -6,25 +6,61 @@ class SignUpForm extends Component {
 	constructor(props) {
 		super(props);
 
-		this.state = {};
+		this.state = {
+			name: "",
+			email: "",
+			password: "",
+			isCorrectName: false,
+		};
 	}
 
 	submitCancel = (e) => {
 		e.preventDefault();
 	};
 
+	inputChangeHndlr = (e) => {
+		this.setState({ [e.target.name]: e.target.value });
+	};
+
 	render() {
+		const { name, email, password, isCorrectName } = this.state;
+
+		const nameStyle = classNames(
+			styles.inputs,
+			isCorrectName ? styles.valid : styles.invalid
+		);
+
 		return (
 			<form onSubmit={this.submitCancel} className={styles.signUpForm}>
 				<label className={styles.formLabel}>
 					Name
-					<input type="text" className={styles.inputs} />
+					<input
+						type="text"
+						className={nameStyle}
+						value={name}
+						name="name"
+						onChange={this.inputChangeHndlr}
+					/>
 				</label>
 				<label className={styles.formLabel}>
-					Email <input type="email" className={styles.inputs} />
+					Email
+					<input
+						type="email"
+						className={styles.inputs}
+						value={email}
+						name="email"
+						onChange={this.inputChangeHndlr}
+					/>
 				</label>
 				<label className={styles.formLabel}>
-					Password <input type="password" className={styles.inputs} />
+					Password
+					<input
+						type="password"
+						className={styles.inputs}
+						value={password}
+						name="password"
+						onChange={this.inputChangeHndlr}
+					/>
 				</label>
 				<button>Sign up</button>
 			</form>

482760caffcecf09111850429365d76671006085
MaxSavchuk <avatar2945@gmail.com>
2021-08-12 22:05:19 +0300

    add corrections in router

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import "./App.css";
 import Header from "./components/Header";
 import HomePage from "./pages/HomePage";
 import Footer from "./components/Footer";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import ComponentsPage from "./pages/ComponentsPage";
 
 function App() {
